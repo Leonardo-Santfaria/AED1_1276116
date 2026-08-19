8d0b06c4891d0a2a491a615f7aae9917bc59a0c8
Leonardo Faria <leonardo.rsfaria91@gmail.com>
2026-08-19 11:22:37 -0300

    Create 1180.c

---
+++ 1180.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+typedef struct{
+    int minor;
+    int pos_minor;
+}Mino;
+
+Mino find_minor(int vet[], int range){
+    Mino x;
+    if(range == 1){
+        x.minor = vet[0];
+        x.pos_minor = 0;
+        return x;
+    }else{
+        x = find_minor(vet, range-1);
+        if(x.minor<vet[range-1]){
+            return x;
+        }else{
+            x.minor = vet[range-1];
+            x.pos_minor = range-1;
+            return x;
+        }
+    }
+    return x;
+}
+
+int main(){
+    int n;
+    Mino m;
+    do{ scanf("%d", &n);    }while(n<=1||n>=1000);
+    int x[n];
+    for(int i=0;i<n;i++){   scanf("%d", &x[i]);}
+    m = find_minor(x, n);
+    printf("Menor valor: %d\n", m.minor);
+    printf("Posicao: %d\n", m.pos_minor);
+}
