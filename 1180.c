5d2f54df6902b3087bd9ae7f199a78aa9d259142
Leonardo Faria <leonardo.rsfaria91@gmail.com>
2026-09-02 08:53:34 -0300

    Update 1180.c

--- 1180.c
+++ 1180.c
@@ -1,3 +1,14 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Algortimo e Estrutura de Dados 2026S1
+Nome        : Leonardo Rafael dos Santos Faria
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1180
+Data        : 02/09/2026
+Objetivo    : Ler quantidade de entradas, seus respectivos valores e retornar o menor número e sua posição
+Dificuldade : --
+Uso de IA   : --
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 
 typedef struct{
