62775023873da0dc965f1a3f1bd19c060b7eb278
Leonardo Faria <leonardo.rsfaria91@gmail.com>
2026-09-02 09:06:59 -0300

    Update 1383.c

--- 1383.c
+++ 1383.c
@@ -1,3 +1,14 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Algortimo e Estrutura de Dados 2026S1
+Nome        : Leonardo Rafael dos Santos Faria
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1383
+Data        : 02/09/2026
+Objetivo    : Ler quantidade de sudokus de entradas, suas respectivas soluções e retornar quais resoluções de sudoku estão certas e quais estão erradas
+Dificuldade : Tive dificuldade para percorrer a matriz verificando cada bloco de 3x3 da mesma e ver se cada número era diferente.
+Uso de IA   : Tive auxílio da IA para me explicar o erros da minha lógica no meu códico ao percorrer a matriz por partes individuais de 3x3.
+-------------------------------------------------------------------------- */
+
 #include <stdio.h>
 int verify_horizontal_vertical(int[][9]);
 int verify_tre_per_tre(int[][9]);
@@ -7,26 +18,20 @@ int main()
     int n;
     scanf("%d", &n);
     int sudokus[9][9], certin[n];
-    for (int num_sudokus = 0; num_sudokus < n; num_sudokus++)
-    {
-        for (int row = 0; row < 9; row++)
-        {
-            for (int col = 0; col < 9; col++)
-            {
+    for (int num_sudokus = 0; num_sudokus < n; num_sudokus++){
+        for (int row = 0; row < 9; row++){
+            for (int col = 0; col < 9; col++){
                 scanf("%d", &sudokus[row][col]);
             }
         }
         certin[num_sudokus] = verify_horizontal_vertical(sudokus) && verify_tre_per_tre(sudokus);
     }
-    for (int num_sudokus = 0; num_sudokus < n; num_sudokus++)
-    {
+    for (int num_sudokus = 0; num_sudokus < n; num_sudokus++){
         printf("Instancia %d\n", num_sudokus + 1);
-        if (certin[num_sudokus] == 1)
-        {
+        if (certin[num_sudokus] == 1){
             printf("SIM\n");
         }
-        else
-        {
+        else{
             printf("NAO\n");
         }
         printf("\n");
@@ -35,20 +40,16 @@ int main()
 
 int verify_horizontal_vertical(int sudokus[][9])
 {
-    for (int row = 0; row < 9; row++)
-    {
+    for (int row = 0; row < 9; row++){
         int seen_row[10] = {0};
         int seen_col[10] = {0};
-        for (int col = 0; col < 9; col++)
-        {
+        for (int col = 0; col < 9; col++){
             int value_row = sudokus[row][col];
             int value_col = sudokus[col][row];
-            if (seen_row[value_row] == 1)
-            {
+            if (seen_row[value_row] == 1){
                 return 0;
             }
-            if (seen_col[value_col] == 1)
-            {
+            if (seen_col[value_col] == 1){
                 return 0;
             }
             seen_row[value_row] = 1;
@@ -61,18 +62,13 @@ int verify_horizontal_vertical(int sudokus[][9])
 int verify_tre_per_tre(int sudokus[][9])
 {
     int seen[10] = {0}, value;
-    for (int row = 0; row < 9; row += 3)
-    {
-        for (int col = 0; col < 9; col += 3)
-        {
+    for (int row = 0; row < 9; row += 3){
+        for (int col = 0; col < 9; col += 3){
             int seen[10] = {0};
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
+            for (int i = 0; i < 3; i++){
+                for (int j = 0; j < 3; j++){
                     value = sudokus[row + i][col + j];
-                    if (seen[value] == 1)
-                    {
+                    if (seen[value] == 1){
                         return 0;
                     }
                     seen[value] = 1;
